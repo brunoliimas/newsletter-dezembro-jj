82b513abc5987a01ff080606327404ac99cc84b5
Bruno Lima <ibrunoliimas@gmail.com>
2024-12-20 07:04:39 -0300

    fixed  bugs

--- scripts/main.js
+++ scripts/main.js
@@ -130,6 +130,10 @@ function customerToggleTab(activeContentId, inactiveContentId, activeTabId, inac
 
 var swiperQualityMonthData = new Swiper(".qualityMonthData", {
      autoHeight: true,
+     autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
     pagination: {
         el: ".swiper-pagination",
         dynamicBullets: true,
